d6d6d1d45cbd29d7b72e0b8d4ea855600faa5e20
Drew Wyatt <drew.j.wyatt@gmail.com>
2016-06-01 23:52:48 -0500

    some webpack optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,32 @@
 var path = require('path')
 var webpack = require('webpack')
+var CommonsChunkPlugin = require('webpack').optimize.CommonsChunkPlugin;
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+	context: __dirname,
 	devtool: 'cheap-module-eval-source-map',
-	entry: [
-		'webpack-hot-middleware/client',
-		'./src/index.tsx'
-	],
+	entry: {
+		'vendor': [
+            'react',
+            'react-dom',
+            'redux',
+            'react-redux'
+        ],
+		'middleware': [
+			'webpack-hot-middleware/client'	
+		],
+		'app': './src/index.tsx'	
+	},
 	output: {
 		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js',
-		publicPath: '/static/'
+		filename: '[name].bundle.js'
 	},
 	plugins: [
+		new CommonsChunkPlugin("vendor", "vendor.bundle.js", Infinity),
 		new webpack.optimize.OccurrenceOrderPlugin(),
-		new webpack.HotModuleReplacementPlugin()
+		new webpack.HotModuleReplacementPlugin(),
+		new HtmlWebpackPlugin({ template: './index.html' })
 	],
 	module: {
         loaders: [
