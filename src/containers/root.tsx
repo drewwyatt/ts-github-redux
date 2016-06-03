b1b6176fbd05e91d9be23f612efa3cb81146d703
Drew Wyatt <drew.j.wyatt@gmail.com>
2016-06-03 16:20:19 -0500

    switched to browser history

--- src/containers/root.tsx
+++ src/containers/root.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import {Router, hashHistory} from 'react-router';
+import {Router, browserHistory} from 'react-router';
 import routes from '../routes';
 
 export class Root extends React.Component<void, void> {
 	render(): JSX.Element {
 		return (
-			<Router history={hashHistory} routes={routes} />
+			<Router history={browserHistory} routes={routes} />
 		);
 	}
 }
